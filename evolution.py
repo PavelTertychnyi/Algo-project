c3213a3fd35cd91a0971c59a661c5c881440feb5
PavelTertychnyi <pavel.tertychny@gmail.com>
2017-01-11 16:32:22 +0200

    Updated evolution

--- evolution.py
+++ evolution.py
@@ -1,4 +1,6 @@
+import math
 import random
+
 
 POP_SIZE = 23
 NUM_OFFSPRINGS = POP_SIZE * 10
@@ -17,21 +19,62 @@ fMin = 0
 
 class Earth:
     def __init__(self):
-        t = random.randint(tMin, tMax)
-        s = random.randint(sMin, sMax)
-        t0 = 20
-        s0 = 1367
-        maxAge = 122
-        c1 = 0.5
-        c2 = 0.5
-        sMaxDif = max(abs(sMax - s0), abs(sMin - s0))
+        self.t = random.randint(tMin, tMax)
+        self.s = random.randint(sMin, sMax)
+        self.t0 = 20
+        self.s0 = 1367
+        self.maxAge = 122
+        self.c1 = 0.5
+        self.c2 = 0.5
+        self.sMaxDif = max(abs(sMax - self.s0), abs(sMin - self.s0))
+        self.tMaxDif = max(abs(tMax - self.t0), abs(tMin - self.t0))
 
 
 class Human:
     def __init__(self):
-        fat = random.randint(fMin, fMax) / 100
-        skinColor = random.randint(cMin, cMax)
-        age = 80
+        self.fat = random.randint(fMin, fMax) / 50.
+        self.skinColor = random.randint(cMin, cMax)
+        self.age = 80
+
+    def setFat(self, fatVal):
+        self.fat = fatVal
+
+    def setSkinColor(self, skinColorVal):
+        self.skinColor = skinColorVal
+
+    def setAge(self, ageVal):
+        self.age = ageVal
+
+
+
+def howMuchLeft(earth, human):
+    F = earth.maxAge - human.age * (1 - 0.5 * abs(earth.s - earth.s0) * (earth.s - earth.s0) * cMax / earth.sMaxDif / earth.sMaxDif / human.skinColor -
+                                        0.5 * abs(earth.t - earth.t0) * (earth.t - earth.t0) / earth.tMaxDif / earth.tMaxDif * human.fat)
+    return F
+
+
+def mutation(population):
+    individual = random.choice(population)
+    mutate_feature = random.randint(0, len(individual) - 1)
+    if mutate_feature == 0:
+        individual[mutate_feature] = random.randint(fMin, fMax)
+    else:
+        individual[mutate_feature] = random.randint(cMin, cMax)
+    population[population.index(individual)] = individual
+    return population
+
+
+def crossover(ind1, ind2):
+    child = tuple()
+    for feature in xrange(len(ind1) - 1):
+        child += ((ind1[feature] + ind2[feature]) / 2.0, )
+    return child
+
+
+def selection(population, size):
+    fitness = []
+    for individual in population:
+        fitness.append()
 
 def randomGeneration(size):
     generation = []
@@ -39,3 +82,8 @@ def randomGeneration(size):
         h = Human()
         generation.append(h)
     return generation
+
+if __name__ == "main":
+    earth = Earth()
+    population = randomGeneration(10)
+
